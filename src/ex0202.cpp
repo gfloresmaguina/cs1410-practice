2bedb4697d8bca2da9e6a5791b957e745c8cb8f9
gfloresmaguina <gfloresmaguina@mail.weber.edu>
2026-01-17 19:10:57 +0000

    done with week 2

---
+++ src/ex0202.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+
+    cout << "Enter day: ";
+    string day;
+    
+    cin >> day;
+
+    if(day == "sun" || day == "sat"){
+        cout << "It's a weekend." << endl;
+    }
+    else{
+        cout << "It's a week day." << endl;
+    }
+
+    return 0;
+}
